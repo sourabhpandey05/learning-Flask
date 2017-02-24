a4a518d69681d729b8735a21e87e7d198aba145c
Sourabh Pandey <sourabhpandey05@gmail.com>
2017-02-24 09:37:21 +0530

    About page

--- routes.py
+++ routes.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 if __name__ =='__main__':
     port= int(os.environ.get('PORT',5000))
     app.run(host='127.0.0.1', port=port,debug=True)
