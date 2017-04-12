06b2060a4b123b49f907fd19db704ea29482aa13
Sourabh <sourabhpandey05@gmail.com>
2017-04-12 09:11:40 +0530

    modification with bootstrap theme

--- routes.py
+++ routes.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
 @app.route("/about")
 def about():
     return render_template("about.html")
